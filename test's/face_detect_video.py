bfe575668ed6bafaec0b9b147caf0e37a61222aa
ashok <ashok_a380@icloud.com>
2021-04-08 10:40:26 -0700

    minor updates

--- test's/face_detect_video.py
+++ test's/face_detect_video.py
@@ -1,23 +1,32 @@
 import cv2
-import numpy as numpy
+import sys
 
-face_recog = cv2.CascadeClassifier("HAAR_cascades/haarcascade_frontalface_default.xml")
-
-cap = cv2.VideoCapture(0)
+faceCascade = cv2.CascadeClassifier("HAAR_cascades/haarcascade_frontalface_default.xml")
+video_capture = cv2.VideoCapture(0)
 
 while True:
-    ret, img = cap.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_recog.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
-    for (x,y,w,h) in faces:
-        cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0, 0), 3)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = img[y:y+h, x:x+w]
-    cv2.imshow("live feed", img)
-    k = cv2.waitKey(30) & 0xff
-    #press esc to quit
-    if k == 27:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+    )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-cap.release()
+# When everything is done, release the capture
+video_capture.release()
 cv2.destroyAllWindows()
